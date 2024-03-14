fe34ba9449d8834803d1b7b15c0767d3ffebc6eb
Ashutoshpython123 <ashutoshcodepro@gmail.com>
2024-03-15 00:31:34 +0530

    added pool info

--- programs/spl-stake/src/lib.rs
+++ programs/spl-stake/src/lib.rs
@@ -12,12 +12,31 @@ pub mod spl_stake {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub admin:Signer<'info>,
+}
 
+
+#[account]
+pub struct PoolInfo {
+    pub admin: Pubkey,
+    pub token: Pubkey,
+    pub start_slot: u64,
+    pub end_slot: u64    
+}
 
 #[account]
 pub struct UserInfo {
     pub amount : u64,
     pub reward_claimed : u64,
     pub deposit_slot : u64
+}
+
+impl UserInfo {
+    pub const LEN: usize = 8 + 8 + 8;
+}
+
+impl PoolInfo  {
+    pub const LEN: usize = 32 + 32 + 8 + 8;
 }
