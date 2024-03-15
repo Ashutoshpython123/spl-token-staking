87093c7d5f162141ce8029a5890deec66f326a95
Ashutoshpython123 <ashutoshcodepro@gmail.com>
2024-03-15 23:53:41 +0530

    initialized the account and pool info

--- programs/spl-stake/src/lib.rs
+++ programs/spl-stake/src/lib.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, MintTo, Transfer};
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,7 +9,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod spl_stake {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, start_slot: u64, end_slot: u64) -> Result<()> {
+        msg!("initialized.");
+
+        let pool_info = &mut ctx.accounts.pool_info;
+        pool_info.admin = ctx.accounts.admin.key();
+        pool_info.token = ctx.accounts.staking_token.key();
+        pool_info.start_slot = start_slot;
+        pool_info.end_slot = end_slot;
         Ok(())
     }
 }
@@ -15,6 +25,13 @@ pub mod spl_stake {
 pub struct Initialize<'info> {
     #[account(mut)]
     pub admin:Signer<'info>,
+    #[account(init, payer = admin, space = 8 + PoolInfo::LEN)]
+    pub pool_info: Account<'info, PoolInfo>,
+    #[account(mut)]
+    pub staking_token: InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub admin_staking_wallet: InterfaceAccount<info, TokenAccount>,
+    pub system_program: Program<'info, System>
 }
 
 
